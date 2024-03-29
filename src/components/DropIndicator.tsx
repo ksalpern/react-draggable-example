fb54b81e7cbfda1e92219b7cf353c8f5c58ea24a
Kseniia <ksenia.pidopryhora@gmail.com>
2024-03-29 19:15:44 +0200

    add drop indicator

---
+++ src/components/DropIndicator.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const DropIndicator = ({
+  beforeId,
+  column,
+}: {
+  beforeId: string | null;
+  column: string;
+}): JSX.Element => {
+  return (
+    <div
+      data-before={beforeId || "-1"}
+      data-column={column}
+      className='my-0.5 h-0.5 w-full bg-violet-400 opacity-0'
+    />
+  );
+};
